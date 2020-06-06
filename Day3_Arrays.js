5159129c39ccef23d80de99b3b5e15caae749a5e
Ashank <54265111+Ashank16@users.noreply.github.com>
2020-06-06 21:58:43 +0530

    Create Day3_Arrays.js

---
+++ Day3_Arrays.js
@@ -0,0 +1,52 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    var first = nums[0];
+    var second = 0;
+    for(let i = 0; i < nums.length; i++){
+        if(first < nums[i]){
+            second = first;
+            first = nums[i];            
+        } 
+        if((second < nums[i]) && (nums[i] < first)){
+            second = nums[i];
+        }       
+    }
+    return second;
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
